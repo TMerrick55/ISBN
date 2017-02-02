23b03ac5cf0ea4e0fc069564cdcdf2a7bcb6409f
TMerrick55 <tmerrick44@gmail.com>
2017-02-02 11:31:43 -0500

    ISBN Project Ver. 3.0

--- ISBN.rb
+++ ISBN.rb
@@ -11,3 +11,21 @@ def remove_spaces(isbn_number)
 	# isbn_number.gsub(' ', '')
 	isbn_number.delete(' ')
 end
+
+def check_for_letters(letters)
+	
+	if !!letters.match(/[a-z, A-Z]/) # !! returns true/false
+		false
+	else
+		true
+	end
+end
+
+def check_for_x(num)
+
+	if num[-1].match(/[0-9 x X]/)
+		true
+	else
+		false
+	end
+end
--- Test_ISBN.rb
+++ Test_ISBN.rb
@@ -26,4 +26,14 @@ class Books_ISBN < Minitest::Test
 		book_num_10 = '123 456 9845'
 		assert_equal('1234569845', remove_spaces(book_num_10))
 	end
+
+	def test_check_for_letters
+		book_num_10 = '456s789447'
+		assert_equal(false, check_for_letters(book_num_10))
+	end
+
+	def test_true_if_X_at_end
+		book_num_10 = 'x43798025x'
+		assert_equal(true, check_for_x(book_num_10))
+	end
 end
