506aeeb47a6c8dc31b77be05d9bafa76d43d21aa
TMerrick55 <tmerrick44@gmail.com>
2017-02-02 10:48:17 -0500

    ISBN Project Ver. 2.0

--- ISBN.rb
+++ ISBN.rb
@@ -1,8 +1,13 @@
-def valid_ISBN(book_num_10)
+def valid_ISBN_length?(book_num_10)
 
 	if book_num_10.length == 10
 		true
 	else
 		false
 	end
+end
+
+def remove_spaces(isbn_number)
+	# isbn_number.gsub(' ', '')
+	isbn_number.delete(' ')
 end
--- Test_ISBN.rb
+++ Test_ISBN.rb
@@ -7,8 +7,23 @@ class Books_ISBN < Minitest::Test
 		assert_equal(1,1)
 	end
 
-	def test_num_is_10_true
-		book_num_10 - '9182736450'
-		assert_equal(true, book_num_10)
+	def test_num_10_is_true
+		book_num_10 = '9182736450'
+		assert_equal(true, valid_ISBN_length?(book_num_10))
+	end
+
+	def test_num_10_is_false
+		book_num_10 = '290834905'
+		assert_equal(false, valid_ISBN_length?(book_num_10))
+	end
+
+	def test_num_is_false_dash
+		book_num_10 = '402 378 91 3'
+		assert_equal('402378913', remove_spaces(book_num_10))
+	end
+
+	def test_removing_spaces
+		book_num_10 = '123 456 9845'
+		assert_equal('1234569845', remove_spaces(book_num_10))
 	end
 end
