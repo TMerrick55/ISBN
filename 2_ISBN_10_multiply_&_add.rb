870484b7631f2a9449a7e790bc97537781249a39
TMerrick55 <tmerrick44@gmail.com>
2017-02-06 16:17:05 -0500

    ISBN-10 Project Ver. 9.5

--- 2_ISBN_10_multiply_&_add.rb
+++ 2_ISBN_10_multiply_&_add.rb
@@ -25,9 +25,9 @@ end
 
 def compare_check_digit(isbn_num)
 	
-	isbn_value = multiply(isbn_num)
-	isbn_total = sum(isbn_value)
-	isbn_mod = remainder(isbn_total)
+	isbn_value = multiply(isbn_num) # function equals variable(backwards)
+	isbn_total = sum(isbn_value)	# function equals variable('')
+	isbn_mod = remainder(isbn_total) # function equals variable('')
 
 	if isbn_mod == 10 && isbn_num[-1].match(/[xX]/)
 		true
